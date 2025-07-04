745939f7cf53b9eccfbf4c57329b7ed282d34dbd
Dianelito <95977637+Dianelito@users.noreply.github.com>
2025-07-04 19:29:51 +0000

    Update LasUpdate

--- language.js
+++ language.js
@@ -1,6 +1,6 @@
 const translations = {
     es: {
-        LastUpdate: "Ultima actualizacion: 03/07/25 1:28 pm",
+        LastUpdate: "Ultima actualizacion: 03/07/25 1:30 pm",
         // index.html
         homeTitle: "Hogar",
         welcomeHeader: "¡Bienvenido!",
@@ -41,7 +41,7 @@ const translations = {
         returnButton: "Regresar",
     },
     en: {
-        LastUpdate: "LastUpdate: 03/07/25 1:28 pm",
+        LastUpdate: "LastUpdate: 03/07/25 1:30 pm",
         // index.html
         homeTitle: "Home",
         welcomeHeader: "Welcome!",
